0d9c4f50e79f356f8a390a47ed4bb4c8125359df
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-23 15:35:16 +0900

    add logic that update dom and format money

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
+getRandomUserData();
+getRandomUserData();
+getRandomUserData();
 
 async function getRandomUserData() {
   const res = await fetch("https://randomuser.me/api");
@@ -14,7 +17,7 @@ async function getRandomUserData() {
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random * 1000000),
+    money: formatMoney(Math.floor(Math.random() * 1000000)),
   };
 
   addData(newUser);
@@ -22,6 +25,23 @@ async function getRandomUserData() {
 
 function addData(newUser) {
   data.push(newUser);
+
+  updateDom();
 }
 
-getRandomUserData();
+function updateDom(privateData = data) {
+  // Clear main div
+  main.innerHTML = " <h2><strong>Person</strong> Wealth</h2>";
+  privateData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    main.appendChild(element);
+  });
+}
+
+function formatMoney(money) {
+  return "$" + money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"); // 12,345.67
+}
+
+addUserBtn.addEventListener("click", getRandomUserData);
